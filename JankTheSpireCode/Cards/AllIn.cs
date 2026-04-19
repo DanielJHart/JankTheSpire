a9263596b708626a70efde72f64329e56cead0d4
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-04-19 10:21:19 +0100

    Adding All In card.

---
+++ JankTheSpireCode/Cards/AllIn.cs
@@ -0,0 +1,27 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace JankTheSpire.JankTheSpireCode.Cards;
+
+[Pool(typeof(IroncladCardPool))]
+public class AllIn() : CustomCardModel(3, CardType.Skill,
+    CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}

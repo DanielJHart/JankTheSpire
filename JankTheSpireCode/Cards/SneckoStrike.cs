bb26e961fb1d1e893b43d216c66dac4bd17564a6
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-04-19 14:52:22 +0100

    Added Snecko Strike and images for all cards so far.

---
+++ JankTheSpireCode/Cards/SneckoStrike.cs
@@ -0,0 +1,39 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JankTheSpire.JankTheSpireCode.Cards;
+
+[Pool(typeof(IroncladCardPool))]
+public class SneckoStrike() : CustomCardModel(4,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AllEnemies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(10, ValueProp.Move)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+    }
+
+    public override Task AfterCardDrawn(PlayerChoiceContext choiceContext, CardModel card, bool fromHandDraw)
+    {
+        if (card == this)
+        {
+            card.EnergyCost.SetThisTurnOrUntilPlayed(card.Owner.RunState.Rng.CombatEnergyCosts.NextInt(4));
+        }
+
+        return base.AfterCardDrawn(choiceContext, card, fromHandDraw);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars["Damage"].UpgradeValueBy(5);
+    }
+}

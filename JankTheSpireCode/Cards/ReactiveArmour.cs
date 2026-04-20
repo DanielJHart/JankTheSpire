d4a1353353ad625c48d28ffc1572320b779ad8b2
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-04-20 20:09:00 +0100

    Adding Reactive Armour

---
+++ JankTheSpireCode/Cards/ReactiveArmour.cs
@@ -0,0 +1,29 @@
+﻿using BaseLib.Utils;
+using JankTheSpire.JankTheSpireCode.Powers;
+using JankTheSpire.JankTheSpireCode.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace JankTheSpire.JankTheSpireCode.Cards;
+
+[Pool(typeof(IroncladCardPool))]
+public class ReactiveArmour() : JankyCardModel(1,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        var cardSource = this;
+        await PowerCmd.Apply<ReactiveArmourPower>(cardSource.Owner.Creature, 1, cardSource.Owner.Creature, cardSource);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.EnergyCost.UpgradeBy(-1);
+    }
+}
---
+++ JankTheSpireCode/Powers/ReactiveArmourPower.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JankTheSpire.JankTheSpireCode.Powers;
+
+public sealed class ReactiveArmourPower : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Single;
+
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props,
+        Creature? dealer, CardModel? cardSource)
+    {
+        if (target == this.Owner
+            && (dealer != null && target == dealer)
+            && this.CombatState.CurrentSide == this.Owner.Side
+            && result.UnblockedDamage > 0)
+        {
+            await PowerCmd.Apply<PlatingPower>(target, result.UnblockedDamage, dealer, null);
+        }
+    }
+}

4545fe210d7c63921063bab61fc0e6770eca331b
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-03 16:13:50 +0100

    Adding Chokehold card and power

---
+++ JankTheSpireCode/Cards/Chokehold.cs
@@ -0,0 +1,41 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using JankTheSpire.JankTheSpireCode.Powers;
+using JankTheSpire.JankTheSpireCode.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JankTheSpire.JankTheSpireCode.Cards;
+
+[Pool(typeof(NecrobinderCardPool))]
+public class Chokehold() : JankyCardModel(1, CardType.Skill,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override bool ShouldGlowRedInternal => this.Owner.IsOstyMissing;
+
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.OstyAttack];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+        [
+            new PowerVar<ChokeholdPower>(6m)
+        ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (play.Target != null)
+        {
+            ChokeholdPower? power = await PowerCmd.Apply<ChokeholdPower>(play.Target, this.DynamicVars["ChokeholdPower"].BaseValue, this.Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars["ChokeholdPower"].UpgradeValueBy(3m);
+    }
+}
---
+++ JankTheSpireCode/Powers/ChokeholdPower.cs
@@ -0,0 +1,69 @@
+﻿using JankTheSpire.JankTheSpireCode.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Monsters;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JankTheSpire.JankTheSpireCode.Powers;
+
+public class ChokeholdPower : JankyPowerModel
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    private Creature? osty;
+
+    public override Task AfterApplied(Creature? applier, CardModel? cardSource)
+    {
+        if (applier != null 
+            && applier.Player != null 
+            && applier.Player.Osty != null)
+        {
+            osty = applier.Player.Osty;
+        }
+        
+        return base.AfterApplied(applier, cardSource);
+    }
+
+    public override Task AfterAttack(AttackCommand command)
+    {
+        if (command.Attacker == osty)
+        {
+            PowerCmd.Remove(this);
+        }
+        
+        return base.AfterAttack(command);
+    }
+
+    public override Task AfterDeath(PlayerChoiceContext choiceContext, Creature creature, bool wasRemovalPrevented, float deathAnimLength)
+    {
+        if (creature == osty)
+        {
+            PowerCmd.Remove(this);
+        }
+        
+        return base.AfterDeath(choiceContext, creature, wasRemovalPrevented, deathAnimLength);
+    }
+
+    public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (side == CombatSide.Player)
+        {
+            await CreatureCmd.Damage(choiceContext, this.Owner, new DamageVar(this.Amount, ValueProp.Move), null, null);
+            if (!this.Owner.IsAlive)
+            {
+                await Cmd.CustomScaledWait(0.1f, 0.25f);
+            }
+        }
+        
+        await base.BeforeTurnEnd(choiceContext, side);
+    }
+    
+}
